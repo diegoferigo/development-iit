d415bd317064bfdaec40e40a56ea90473d4a96c3
diegoferigo <diego.ferigo@iit.it>
2018-07-20 18:56:06 +0200

    Minor edits in entrypoint.sh scripts

--- Development/entrypoint.sh
+++ Development/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z "$(which setup_development.sh)" ] ; then
+if [ ! -x "$(which setup_development.sh)" ] ; then
     echo "File setup_development.sh not found."
     exit 1
 fi
 
+# Initialize the container
 setup_development.sh
 
 # If a CMD is passed, execute it
