c60591d9ed7dd9ce80777bab1ef9bca312b318b5
Diego Ferigo <diego.ferigo@iit.it>
2018-08-03 11:38:22 +0200

    Improved resources chown-ing step

--- Development/setup.sh
+++ Development/setup.sh
@@ -13,16 +13,20 @@ echo "source /home/$USERNAME/.bashrc-dev" >> /home/${USERNAME}/.bashrc
 echo "source /home/$USERNAME/.bashrc-dev" >> /root/.bashrc
 
 # Change the permission of all persistent resources mounted inside $HOME.
-# If you don't to get a chowned resource, mount it somewhere else.
-RESOURCES_MOUNTED_IN_HOME=($(mount | tr -s " " | cut -d " " -f 3 | grep /home/${USERNAME}))
-for resource in ${RESOURCES_MOUNTED_IN_HOME[@]} ; do
-    echo "Changing ownership of $resource"
-	chown -R ${USERNAME}:${USERNAME} $resource
-done
+# If you don't want to get a chowned resource, mount it somewhere else.
+if [ -n "$(mount | tr -s " " | cut -d " " -f 3 | grep /home/${USERNAME})" ] ; then
+    echo "Fixing mounted volumes permissions"
+    declare -a RESOURCES_MOUNTED_IN_HOME
+    RESOURCES_MOUNTED_IN_HOME=($(mount | tr -s " " | cut -d " " -f 3 | grep /home/${USERNAME}))
+    for resource in ${RESOURCES_MOUNTED_IN_HOME[@]} ; do
+        echo " -> Changing ownership of $resource"
+        chown -R ${USERNAME}:${USERNAME} $resource
+    done
+fi
 
 # Change the permissions of .local and .config
-[ -d /home/${USERNAME}/.local ] && chown ${USERNAME}:${USERNAME} /home/${USERNAME}/.local
-[ -d /home/${USERNAME}/.config ] && chown ${USERNAME}:${USERNAME} /home/${USERNAME}/.config
+[ -d /home/${USERNAME}/.local ] && chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.local
+[ -d /home/${USERNAME}/.config ] && chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.config
 
 # Configure git
 if [[ ! -z ${GIT_USER_NAME:+x} && ! -z ${GIT_USER_EMAIL:+x} ]] ; then
@@ -41,12 +45,13 @@ if [[ ! -z ${GIT_USER_NAME:+x} && ! -z ${GIT_USER_EMAIL:+x} ]] ; then
 	echo "... Done"
 fi
 
-# Fix permissions of the IIT directory and link it into the user's home
-if [ -d ${IIT_DIR} ] ; then
-	chown -R $USERNAME:$USERNAME ${IIT_DIR}
-	if [ ! -d /home/$USERNAME/$IIT_DIR ] ; then
-		su -c "ln -s ${IIT_DIR} /home/$USERNAME/$IIT_DIR" $USERNAME
-	fi
+# Fix permissions of the IIT directory
+CHOWN_SOURCES=${CHOWN_SOURCES:-0}
+if [[ ${CHOWN_SOURCES} -eq 1 && -d ${IIT_DIR} ]] ; then
+    # Do this in background since it might be a slow operation if the folder is big
+	chown $USERNAME:$USERNAME ${IIT_DIR}
+	chown -R $USERNAME:$USERNAME ${IIT_DIR}/sources &
+	chown -R $USERNAME:$USERNAME ${IIT_DIR}/local &
 fi
 
 # Configure YARP namespace
