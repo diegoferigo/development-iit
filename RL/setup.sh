c193e335a0fd65307c4121ce0e2e53a5c53b6f85
Diego Ferigo <diego.ferigo@iit.it>
2019-10-16 14:41:35 +0200

    Install Julia and Juno

--- RL/setup.sh
+++ RL/setup.sh
@@ -10,3 +10,27 @@ if [ -x "$(which setup_development.sh)" ] ; then
     # Setup git for the runtime user
     su -c "git config --global remote-hg.ignore-name '~|pre|pendulum'" $USERNAME
 fi
+
+# =================
+# SETUP ATOM / JUNO
+# =================
+
+ATOM_FOLDER="/home/$USERNAME/.atom"
+
+# Create the .atom folder if it does not exist
+if [ ! -d ${ATOM_FOLDER} ] ; then
+    su -c "mkdir -p ${ATOM_FOLDER}" $USERNAME
+fi
+
+# Check if it is empty
+set +e +o pipefail
+find ${ATOM_FOLDER} -mindepth 1 | read
+
+# If empty, copy the default packages
+if [ $? -eq 1 ] ; then
+    echo "==> Copying atom folder with default packages in the user home"
+    cp -r /opt/dotatom/* /home/$USERNAME/.atom/
+    chown -R $USERNAME:$USERNAME /home/$USERNAME/.atom
+fi
+
+set -eu -o pipefail
