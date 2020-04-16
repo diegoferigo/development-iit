7f3a39907cd8c44d4354f16ff0e5adfa4c917667
Diego Ferigo <diego.ferigo@iit.it>
2020-04-16 10:54:32 +0200

    Setup atom only if found

--- RL/setup.sh
+++ RL/setup.sh
@@ -11,26 +11,30 @@ if [ -x "$(which setup_development.sh)" ] ; then
     su -c "git config --global remote-hg.ignore-name '~|pre|pendulum'" $USERNAME
 fi
 
-# =================
-# SETUP ATOM / JUNO
-# =================
+function setup_atom()
+{
+    # Create the .atom folder if it does not exist
+    if [ ! -d ${ATOM_FOLDER} ] ; then
+        su -c "mkdir -p ${ATOM_FOLDER}" $USERNAME
+    fi
 
-ATOM_FOLDER="/home/$USERNAME/.atom"
+    # Check if it is empty
+    set +e +o pipefail
+    find ${ATOM_FOLDER} -mindepth 1 | read
 
-# Create the .atom folder if it does not exist
-if [ ! -d ${ATOM_FOLDER} ] ; then
-    su -c "mkdir -p ${ATOM_FOLDER}" $USERNAME
+    # If empty, copy the default packages
+    if [ $? -eq 1 ] ; then
+        echo "==> Copying atom folder with default packages in the user home"
+        cp -r ${IMAGE_ATOM_DOTFOLDER}/* /home/$USERNAME/.atom/
+        chown -R $USERNAME:$USERNAME /home/$USERNAME/.atom
+    fi
+
+    set -eu -o pipefail
+    return 0
+}
+
+IMAGE_ATOM_DOTFOLDER="/opt/dotatom"
+
+if [-d ${IMAGE_ATOM_DOTFOLDER}] ; then
+    setup_atom
 fi
-
-# Check if it is empty
-set +e +o pipefail
-find ${ATOM_FOLDER} -mindepth 1 | read
-
-# If empty, copy the default packages
-if [ $? -eq 1 ] ; then
-    echo "==> Copying atom folder with default packages in the user home"
-    cp -r /opt/dotatom/* /home/$USERNAME/.atom/
-    chown -R $USERNAME:$USERNAME /home/$USERNAME/.atom
-fi
-
-set -eu -o pipefail
